9669efdc77ff4f4e3c63c68bccef61f093776f4c
unaicastillo <unaicastillocarrillo@gmail.com>
2025-01-25 17:29:31 +0100

    FindUserDone

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,11 +1,19 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class Biblioteca {
-    public static Random rd = new Random();
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        GestionUsuarios gestor=new GestionUsuarios();
+        Persona user1 = new Persona("unai", "1234", false);
+        Persona user2 = new Persona("dario", "5678", false);
+        Persona user3 = new Persona("no", "9012", false);
+        Persona admin1 = new Persona("babuku", "3456", true);
+        Persona admin2 = new Persona("pablo", "7890", true);
+        System.out.println("Escribe tu nombre");
+            
 
-
-
-
-
+        gestor.buscarUser();
+        
+    }
 
 }
--- src/GestionUsuarios.java
+++ src/GestionUsuarios.java
@@ -1,22 +1,51 @@
-public class GestionUsuarios{
-   private Persona usuario;
-   private Persona[] registro=new Persona[100];
-   public GestionUsuarios(){
-      usuario.setNombre("nombre");
-      usuario.setContrasena("conn");
-      usuario.setAdmin(false);
+import java.util.Scanner;
+
+public class GestionUsuarios {
+   public static Scanner sc = new Scanner(System.in);
+   private Persona persona = new Persona();
+
+   private Persona[] registro;
+   private int posicion;
+
+   public GestionUsuarios() {
+      this.registro = new Persona[100];
+      this.posicion = 0;
    }
 
-   public void registrarUsuarios(){
-      for (int i = 0; i < registro.length; i++) {
-         registro[i]= usuario;
+   public void buscarUser() {
+      String contrasena;
+      String nombre;
+      boolean isUser = false;
+
+      while (isUser == false) {
+
+         nombre = sc.nextLine();
+
+         if (nombre.equals(persona.getNombre())) {
+            System.out.println("Escribe tu contraseña");
+            contrasena = sc.nextLine();
+            if (contrasena.equals(persona.getContrasena())) {
+
+            }
+         } else {
+            System.out.println("ese usuario no existe pruebe otra vez");
+         }
+      }
+
+   }
+
+   public void registrarUsuarios(Persona usuariPersona, Persona usuarioActivo) {
+      if (usuariPersona != null) {
+         registro[posicion] = usuariPersona;
+         posicion++;
       }
    }
-   public void mostrarUser(){
-      for (int i = 0; i < registro.length; i++) {
-         System.err.println(registro[i]);
+
+   public void mostrarUser(Persona usuarioActivo) {
+      for (int i = 0; i < posicion; i++) {
+         System.out.println(registro[i]);
       }
-      
+
    }
 
 }
