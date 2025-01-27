ca71965130e217818aedf36755e979202d8a31ef
cabraDeGuerra <dariosimonrecio@gmail.com>
2025-01-27 17:07:41 +0100

    Lista de libros predeterminados

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,16 @@
 public class Biblioteca {
 
+    private int tam;
+    private int[] libros=new int[tam];
+    
+
 
 }
+    public Biblioteca(){
+
+    }
+
+
+    /*Agregar Libros nuevos */
+
+    /*Eliminar libros existentes */
